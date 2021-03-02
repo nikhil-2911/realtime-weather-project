09044edf9a2ed0811f267d01d367f06a6e8dc6bb
nikhil-2911 <nikhilxmr5@gmail.com>
2021-03-03 00:34:53 +0530

    weather-app first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap : {
+        BASE_URL : "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY : "2e75518821e417e51e5cebdd8758aebc"
+    }
+}
+
+module.exports = constants;
